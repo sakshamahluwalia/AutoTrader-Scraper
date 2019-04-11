5d4d847e6338a5fc5dd83a4c8bfd28b0b3dfec58
ahluwa41 <saksham.ahluwalia@mail.utoronto.ca>
2019-04-10 20:22:24 -0400

    added functionality to iterate through all the pages but need a better way to get name and year

--- app.py
+++ app.py
@@ -2,25 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox(executable_path=r'./driver/geckodriver')
-driver.get('https://www.autotrader.ca/cars/?rcp=100&rcs=0') # update rcs by 100 each time
 
-cars = driver.find_elements(By.CLASS_NAME, "result-item-inner")
+count = 0
 
 lst = []
+while (count != 200):
 
-for car in cars:
-    info = {}
-    info["Car"] = str(car.find_elements(By.CLASS_NAME, "result-title")[0].text)
-    info["year"] = str(car.find_elements(By.CLASS_NAME, "result-title")[0].text)[:4]
-    if (car.find_elements(By.CLASS_NAME, "kms") != []):
-        if (str(car.find_elements(By.CLASS_NAME, "kms")[0].text) != ""):
-            info["Mileage"] = str(car.find_elements(By.CLASS_NAME, "kms")[0].text)[len("Mileage "):-3]
-    if (car.find_elements(By.CLASS_NAME, "price-amount") != []):
-        if (str(car.find_elements(By.CLASS_NAME, "price-amount")[0].text) != ""):
-            info["price"] = str(car.find_elements(By.CLASS_NAME, "price-amount")[0].text)
-    lst.append(info)
+	driver.get('https://www.autotrader.ca/cars/?rcp=100&rcs={}'.format(count)) # update rcs by 100 each time
+	cars = driver.find_elements(By.CLASS_NAME, "result-item-inner")
 
-print(lst)
+	for car in cars:
+	    info = {}
+	    info["Car"] = car.find_elements(By.CLASS_NAME, "result-title")[0].text
+	    # info["year"] = str(car.find_elements(By.CLASS_NAME, "result-title")[0].text)[:4]
+	    if (car.find_elements(By.CLASS_NAME, "kms") != []):
+		if (str(car.find_elements(By.CLASS_NAME, "kms")[0].text) != ""):
+		    info["Mileage"] = str(car.find_elements(By.CLASS_NAME, "kms")[0].text)[len("Mileage "):-3]
+	    if (car.find_elements(By.CLASS_NAME, "price-amount") != []):
+		if (str(car.find_elements(By.CLASS_NAME, "price-amount")[0].text) != ""):
+		    info["price"] = str(car.find_elements(By.CLASS_NAME, "price-amount")[0].text)
+	    lst.append(info)
+
+	count += 100
+
+print(len(lst))
 
 
     
